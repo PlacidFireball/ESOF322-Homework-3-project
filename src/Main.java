b7dfae48574802c5bab31c757dea8822e5b3080a
placidfireball <jared.lee.weiss@gmail.com>
2021-10-06 10:55:11 -0600

    Finish development of command line app, apologies for any bugs that I missed.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,21 @@
+/*
+* ESOF322 Homework 3
+* Completed by: Jared Weiss (no partners) for Professor Faqeer al Rehman
+* Implements changing algorithms on the fly. (the Strategy Pattern)
+* Compilation instructions:
+* I assume you have some jdk installed (I used openjdk-11) so you can build this.
+* I programmed this in Intellij, if you would like to clone this code,
+* you can clone it with
+* $ git clone https://github.com/PlacidFireball/ESOF322-Homework-3-project
+* and build it in IntelliJ.
+* Alternatively, this source file can be compiled in the command line like so:
+* $ javac Main.java
+* $ java Main
+* (to clean class files)
+* $ rm *.class
+* */
+
+
 import java.util.*;
 
 public class Main {
@@ -33,24 +51,30 @@ public class Main {
     }
 
     abstract class Inventory {
-        /* whatever needs to go in the inventory (i.e. A list of InventoryObjects?) */
-        Sort defaultSort = new BubbleSort();
-        Sort strategy = defaultSort;
+        Sort strategy;
+        Inventory() {
+            this.strategy = new BubbleSort();
+        }
+        Sort getSortStrategy() {
+            return this.strategy;
+        }
+        void setSortStrategy(Sort strategy) {
+            this.strategy = strategy;
+        }
     }
 
     class InventoryModule extends Inventory {
-        void setStrategy(Sort newStrategy) {
-            strategy = newStrategy;
-        }
+
     }
 
     Main() {
         final String help = "ESOF 322 Homework 3 Commands\n" +
-                "inventory <inventory number:int> - create a new inventory module\n" +
+                "inventory new - create a new inventory module\n" +
                 "sort <inventory number: int> - sort inventory module <inventory number>\n" +
-                "set <inventory number: int> <bubble, insertion, quick, merge> - set the default\n\t\tsorting method on inventory module <inventory number>\n" +
+                "set <inventory number: int> <bubble, insertion, quick, merge> - set the\n\t\tsorting method on inventory module <inventory number>\n" +
                 "help - display this prompt\n" +
                 "exit - quit the program";
+        final String prompt = "Homework 3> ";
         System.out.println(help);
         Scanner scanner = new Scanner(System.in);
         String userInput;
@@ -58,65 +82,75 @@ public class Main {
         LinkedList<InventoryModule> inventoryList = new LinkedList<>();
         boolean exit = false;
         while(!exit) {
+            System.out.println(prompt);
             userInput = scanner.nextLine();
             tokens = userInput.split(" ");
-            if (tokens[0].equalsIgnoreCase("help")) {
-                System.out.println(help);
-            }
+            // help
+            if (tokens[0].equalsIgnoreCase("help")) { System.out.println(help); }
+            // exit
             else if (tokens[0].equalsIgnoreCase("exit")) {
                 System.out.println("Exiting...");
                 exit = true;
             }
+            // set the strategy for an inventory module
             else if (tokens[0].equalsIgnoreCase("set")) {
                 if (tokens.length != 3) System.out.println("Usage:\n\tset <inventory number: int> <bubble, insertion, quick, merge>");
                 else {
                     try {
-                        int inventoryNumber = Integer.parseInt(tokens[1]);
-                        if (inventoryList.get(inventoryNumber) == null || inventoryNumber >= inventoryList.size()) {
-                            throw new NumberFormatException();
+                        int inventoryNumber = Integer.parseInt(tokens[1]); // grab which inventory module we are working with
+                        if (inventoryNumber > 0 && inventoryNumber <= inventoryList.size()) { // make sure we have a valid inventory number
+                            InventoryModule inventory = inventoryList.get(inventoryNumber-1); // select the inventory module we are working with
+                            // set sort method based on user input
+                            if (tokens[2].equalsIgnoreCase("bubble")) { inventory.setSortStrategy(new BubbleSort());}
+                            else if(tokens[2].equalsIgnoreCase("insertion")) { inventory.setSortStrategy(new InsertionSort()); }
+                            else if(tokens[2].equalsIgnoreCase("quick")) { inventory.setSortStrategy(new QuickSort()); }
+                            else if(tokens[2].equalsIgnoreCase("merge")) { inventory.setSortStrategy(new MergeSort()); }
+                            // otherwise report that an invalid sort method has been selected
+                            else { System.out.printf("Invalid sort method: %s\n", tokens[2]); }
                         }
-                        String sortType = null;
-                        if (tokens[2].equals("bubble") || tokens[2].equals("insertion") || tokens[2].equals("quick") || tokens[2].equals("merge")) {
-                            sortType = tokens[2];
+                        else {
+                            System.out.printf("Invalid inventory number: %d\n\tValid values between %d and %d.\n",
+                                    inventoryNumber, 1, inventoryList.size());
                         }
-                        if (sortType == null) {
-                            System.out.println("You did not select a valid sort type.");
-                            System.out.println("Usage:\n\tset <inventory number: int> <bubble, insertion, quick, merge>");
-                        }
-                    } catch (NumberFormatException e) {
+                    } catch (NumberFormatException e) { // catch invalid number parse
                         System.out.println("You did not select a valid inventory number.");
                         System.out.println("Usage:\n\tset <inventory number: int> <bubble, insertion, quick, merge>");
                     }
                 }
             }
-            else if (tokens[0].equalsIgnoreCase("inventory")) {
-                if (tokens.length != 2) System.out.println("Usage:\n\tinventory <inventory number: int>");
-                else {
-                    try {
-                        int inventoryNumber = Integer.parseInt(tokens[1]);
-                        inventoryList.add(inventoryNumber, new InventoryModule());
-                    } catch (NumberFormatException e) {
-                        System.out.println("You did not select a valid inventory number.");
-                        System.out.println("Usage:\n\tinventory <inventory number: int>");
-                    }
-                }
+            // creation of new inventory module
+            else if (tokens[0].equalsIgnoreCase("inventory") && tokens[1].equalsIgnoreCase("new")) {
+                inventoryList.push(new InventoryModule());
+                System.out.printf("Created new inventory, access by doing: set %d <sort type> or sort %d\n", inventoryList.size(), inventoryList.size());
             }
+            // call the sort method of a specific inventory module
             else if (tokens[0].equalsIgnoreCase("sort")) {
                 if (tokens.length != 2) System.out.println("Usage:\n\tsort <inventory number: int>");
                 else {
                     try {
-                        int inventoryNumber = Integer.parseInt(tokens[1]);
+                        int inventoryNumber = Integer.parseInt(tokens[1]); // select what inventory module we are working with
+                        if (inventoryNumber > 0 && inventoryNumber <= inventoryList.size()) {
+                            InventoryModule inventory = inventoryList.get(inventoryNumber-1);
+                            Sort strategy = inventoryList.get(inventoryNumber-1).getSortStrategy();
+                            strategy.sort(inventory);
+                        }
+                        else {
+                            System.out.printf("You did not enter a valid inventory number.\n\tValid values between %d and %d.\n", 1, inventoryList.size());
+                        }
                     } catch (NumberFormatException e) {
-                        System.out.println("You did not select a valid inventory number.");
+                        System.out.println("You did not enter a valid inventory number.");
                         System.out.println("Usage:\n\tsort <inventory number: int>");
                     }
                 }
+            }
+            else {
+                System.out.printf("Invalid command: %s\n\tType \"help\" for help.", userInput);
             }
         }
     }
 
 
     public static void main(String[] args) {
-        new Main();
+        Main main = new Main();
     }
 }
